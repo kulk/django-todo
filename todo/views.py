2b44d64c8ba4de3d828c8cc6e32847a92ad01b89
kulk <roelandkulk@gmail.com>
2019-03-28 21:17:53 +0100

    added form function

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
+from .forms import TaskForm
 
 #@login_required
 def index(request):
-	tasks = Task.objects.all()#author=request.user
-	return render(request, 'todo/index.html', {'tasks': tasks})
+	if request.method == "POST":
+		form = TaskForm(request.POST)
+		if form.is_valid():
+			task = form.save(commit=False)
+			task.user = request.user
+			task.save()
+			return redirect('index')
+	else:
+		form = TaskForm()
+	tasks = Task.objects.all()
+	return render(request, 'todo/index.html', {'form': form, 'tasks': tasks})
